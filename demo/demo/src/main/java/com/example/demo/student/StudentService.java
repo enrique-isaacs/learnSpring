7346c1e3c8b9378e917e0bd452765cef3a1bd96f
Enrique Isaacs <enisaac022_student.wethinkcode.co.za>
2023-05-27 11:26:36 +0200

    Created Service layer by creating a Student Service.

--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service // to make sure this class should be instantiated before being used
 public class StudentService {
 
     public List<Student> getStudents(){
